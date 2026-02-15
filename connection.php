ebaf6c652f7b39806a2550c15e60dfc41de34e0b
Natalia-Timote <nataliamirian.nt@gmail.com>
2026-02-15 19:02:21 -0300

    Create table students

--- connection.php
+++ connection.php
@@ -4,3 +4,5 @@ $databasePath = __DIR__ . '/database.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
 echo 'Connected';
+
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birdth_date TEXT);');
