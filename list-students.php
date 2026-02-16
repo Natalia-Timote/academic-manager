d62f3a9fe12de7ae4c94380d5180378be754f37b
Natalia-Timote <nataliamirian.nt@gmail.com>
2026-02-15 22:00:19 -0300

    Using fetch and while loops to retrieve data from the table

--- list-students.php
+++ list-students.php
@@ -7,9 +7,20 @@ use Alura\Pdo\Domain\Model\Student;
 $databasePath = __DIR__ . '/database.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$statement = $pdo->query('SELECT * FROM students;');
-$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
-$studentList = [];
+$statement = $pdo->query('SELECT * FROM students WHERE id = 1;');
+
+while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $student = new Student(
+        $studentData['id'], 
+        $studentData['name'], 
+        new DateTimeImmutable($studentData['birth_date'])
+    );
+
+    echo $student->age() . PHP_EOL;
+};
+exit();
+
+var_dump($studentDataList);
 
 foreach ($studentDataList as $studentData) {
     $studentList[] = new Student(
