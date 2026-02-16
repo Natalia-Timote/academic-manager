6dc0d3416cc5ee85ff92f284935707a374969d94
Natalia-Timote <nataliamirian.nt@gmail.com>
2026-02-15 22:01:36 -0300

    Returning to fetchAll

--- list-students.php
+++ list-students.php
@@ -7,20 +7,9 @@ use Alura\Pdo\Domain\Model\Student;
 $databasePath = __DIR__ . '/database.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$statement = $pdo->query('SELECT * FROM students WHERE id = 1;');
-
-while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
-    $student = new Student(
-        $studentData['id'], 
-        $studentData['name'], 
-        new DateTimeImmutable($studentData['birth_date'])
-    );
-
-    echo $student->age() . PHP_EOL;
-};
-exit();
-
-var_dump($studentDataList);
+$statement = $pdo->query('SELECT * FROM students;');
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
 
 foreach ($studentDataList as $studentData) {
     $studentList[] = new Student(
