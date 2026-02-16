e377eb710eb4bc8b7e0c0a27041ebae8675b380b
Natalia-Timote <nataliamirian.nt@gmail.com>
2026-02-15 23:40:52 -0300

    Making code more readable

--- add-student.php
+++ add-student.php
@@ -7,12 +7,12 @@ use Alura\Pdo\Domain\Model\Student;
 $databasePath = __DIR__ . '/database.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Lucas Rocha', new DateTimeImmutable('1997-02-26'));
+$student = new Student(null, 'Amélia', new DateTimeImmutable('1997-02-26'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?)";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date)";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+$statement->bindValue('name', $student->name());
+$statement->bindValue('birth_date', $student->birthDate()->format('Y-m-d'));
 
 if($statement->execute()) {
     echo 'Aluno incluído';
